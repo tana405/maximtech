52c1504e285920691d093aa089f3c68204b1b7d9
tana405 <trunova.am@gmail.com>
2024-10-22 21:39:51 +0500

    add menu, html-files, css, rename dir, write some information and func in main.py

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+
+@app.route('/excursion')
+def excursion():
+    return render_template('excursion.html')
+
+
+@app.route('/school')
+def school():
+    return render_template('school.html')
+
+
+@app.route('/student')
+def student():
+    return render_template('student.html')
 
 
 if __name__ == '__main__':
